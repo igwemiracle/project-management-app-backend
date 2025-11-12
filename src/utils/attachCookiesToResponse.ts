5f951fa3b429ae8bc762f92476f628be1e4d6af4
igwemiracle <igwemiracle35@gmail.com>
2025-11-12 09:05:10 +0100

    fixing auth issues

--- src/utils/attachCookiesToResponse.ts
+++ src/utils/attachCookiesToResponse.ts
@@ -8,15 +8,12 @@ interface TokenUser {
 
 export const attachCookiesToResponse = (res: Response, user: TokenUser): void => {
   const token = generateToken(user);
-
-  // const isProduction = process.env.NODE_ENV === "production";
-  // console.log("Cookie Mode:", isProduction ? "PRODUCTION" : "DEVELOPMENT");
+  console.log("Cookie headers:", res.getHeaders()["set-cookie"]);
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true, // true on HTTPS
-    // sameSite: isProduction ? "none" : "lax",
+    secure: true,
     sameSite: "none",
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    maxAge: 24 * 60 * 60 * 1000,
   });
 };
